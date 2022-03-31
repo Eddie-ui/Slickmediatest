6501fa1b010ae7c193f2feda7a928334f605a6ac
EDDIE <70353768+Eddie-ui@users.noreply.github.com>
2022-03-31 15:34:26 +0100

    update

--- src/search/search.jsx
+++ src/search/search.jsx
@@ -1,21 +1,29 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import SearchCtn from "../styles";
+import "./style.css";
 import axios from "axios";
 import { Form } from "../styles/styles";
 const SearchPage = () => {
   const [searchFormData, setSearchFormData] = useState({
     t: "",
-    
     apikey: "aa6b1ac7",
   });
   const test = {
-  
     i: "tt3896198",
     apikey: "aa6b1ac7",
   };
   const [searchStart, setSearchStart] = useState(false);
   const [loader, setLoader] = useState(false);
-  const [searchData, setSearchData] = useState({});
+  const [searchData, setSearchData] = useState(false);
+  const [resError, setResError] = useState(false);
+
+  useEffect(() => {
+    let checklen = searchFormData.t;
+    if (checklen.length < 2) {
+      setSearchStart(false);
+      setLoader(false);
+    }
+  });
   const HandleInputChangeKey = (target) => {
     setSearchFormData((prev) => ({
       ...prev,
@@ -25,19 +33,36 @@ const SearchPage = () => {
     console.log("input key changed", searchFormData);
   };
   const InitSearch = async (e) => {
-    setLoader(true)
-    setSearchStart(true)
+    let checklen = searchFormData.t;
     e.preventDefault();
     console.log("all info", searchFormData, "loader", loader);
-    if (true) {
-      return await postRequest(test)
+    if (checklen.length >= 2) {
+      setLoader(true);
+      setSearchStart(true);
+      setResError(false);
+      setSearchData(false);
+      return await postRequest(searchFormData)
         .then((res) => {
-           setLoader(false);
-          console.log("res", res);
+          console.log("res dtaa", res.data.Response);
+          if (res.data.Response) {
+            setLoader(false);
+            setSearchData(res.data);
+            console.log("res", res.data);
+          }
+          if (res.data.Response == "False") {
+            setResError(res.data.Error);
+            setLoader(false);
+            setSearchStart(true);
 
+            console.log("Entered a Falsy");
+            setSearchData(false);
+          }
         })
         .catch((error) => {
-           setLoader(false);
+          setLoader(false);
+          setSearchStart(true);
+            console.log("Entered an Error");
+          setResError("Error getting result please try again");
         });
     }
   };
@@ -59,8 +84,12 @@ const SearchPage = () => {
               }}
               autoFocus={true}
             />
-            <p className="d-none">i couldnt use this api, as it says i have to become a patron which i have tried and the web doesnt load, also i dont see how i can apply this api to fetch by category when the api only fetches one search result
-            , wink wink, you found the easter egg in my code</p>
+            <p className="d-none">
+              i couldnt use this api, as it says i have to become a patron which
+              i have tried and the web doesnt load, also i dont see how i can
+              apply this api to fetch by category when the api only fetches one
+              search result , wink wink, you found the easter egg in my code
+            </p>
             <ul className={searchStart ? "dropdown-menu show" : "dropdown"}>
               {loader ? (
                 <div className="loader-ctn">
@@ -68,8 +97,39 @@ const SearchPage = () => {
                     <span className="sr-only">Loading...</span>
                   </div>
                 </div>
-              ) : ""}
-             
+              ) : (
+                ""
+              )}
+              {searchData ? (
+                <div className="search-res-ctn">
+                  <div
+                    className="movie-item"
+                    style={{ background: `url(${searchData.Poster})` }}
+                  ></div>
+                  <div className="info-ctn">
+                    <h3>
+                      <span className="label">Title:</span> {searchData.Title}
+                    </h3>
+                    <h5>
+                      <span className="label">Release Date: </span>
+                      {searchData.Released}
+                    </h5>
+                    <h5>
+                      {" "}
+                      <span className="label">IMDB Rating: </span>
+                      {searchData.imdbRating}
+                    </h5>
+                    <h5>
+                      {" "}
+                      <span className="label">Genre: </span>
+                      {searchData.Genre}
+                    </h5>
+                  </div>
+                </div>
+              ) : (
+                <p className="text-center mt-3">{resError}</p>
+              )}
+              {/* {resError ? <p>{resError}</p> : ""} */}
             </ul>
           </div>
         </Form>
@@ -83,6 +143,7 @@ const SearchPage = () => {
 export default SearchPage;
 
 export const postRequest = (payload) => {
-  
-  return axios.post("https://www.omdbapi.com", JSON.stringify(payload));
+  return axios.post(
+    `https://www.omdbapi.com?t=${payload.t}&apikey=${payload.apikey}`
+  );
 };
